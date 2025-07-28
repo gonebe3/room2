b795c7b091fa23c48ce05e30096f105b66ab0bae
gonebe3 <dominikkostiuszkin@gmail.com>
2025-07-28 18:41:48 +0300

    Pridėtas slaptažodžio hash util ir validatorius

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -1,12 +1,17 @@
 import re
 from wtforms.validators import ValidationError
 
+# Slaptažodžio stiprumo regex: didžioji, mažoji, skaičius, spec. simbolis, min 8 simboliai
 password_pattern = re.compile(
     r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
 )
 
-def password_regex(form, field):
+def validate_password_strong(form, field):
+    """
+    Tikrina ar slaptažodis pakankamai stiprus.
+    Naudoti RegisterForm ir ChangePasswordForm.
+    """
     if not password_pattern.match(field.data):
         raise ValidationError(
-            'Password must be at least 8 characters long, include uppercase, lowercase, number, and special character.'
+            "Slaptažodis turi būti bent 8 simbolių, turėti didžiąją, mažąją, skaičių ir specialų simbolį."
         )
