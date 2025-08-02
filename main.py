f2019d61ff840f7852eb374efaa7ce8e9dff4aa6
Simonasvaicaitis <vaic.simonas@gmail.com>
2025-08-02 23:42:54 +0300

    kategorijos pop up menu, neprisijungus krepselio nerodo, logout mygtukas userio profilyje veikia

--- main.py
+++ main.py
@@ -27,6 +27,12 @@ def create_app():
     from datetime import datetime
 
     @app.context_processor
+    def inject_categories():
+        from app.models.category import Category
+        categories = Category.query.order_by(Category.name).all()
+        return dict(categories=categories)
+
+    @app.context_processor
     def inject_now():
         return {'now': datetime.now()}
 
